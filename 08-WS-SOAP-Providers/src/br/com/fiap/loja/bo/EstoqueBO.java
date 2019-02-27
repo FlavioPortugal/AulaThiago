77022f12f05e2094b135b1a87341cde7142f4d01
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-27 20:52:41 -0300

    adiciona 08

---
+++ 08-WS-SOAP-Providers/src/br/com/fiap/loja/bo/EstoqueBO.java
@@ -0,0 +1,39 @@
+package br.com.fiap.loja.bo;
+
+
+
+import org.apache.axis2.AxisFault;
+import org.apache.log4j.Logger;
+
+import br.com.fiap.loja.TerminalConsulta;
+import br.com.fiap.loja.exception.NotFoundException;
+import br.com.fiap.loja.to.ProdutoTO;
+
+// Camada de Regras de Negócio
+public class EstoqueBO {
+	
+	private static Logger log = Logger.getLogger(TerminalConsulta.class);
+
+	public ProdutoTO consultarProduto(int codigo) throws AxisFault {
+		
+		ProdutoTO produto;
+		
+		log.debug("Codigo pesquisando: " + codigo);
+		
+		switch (codigo) {
+		case 401:
+			produto = new ProdutoTO(401, 10, 100, "Camiseta Branca");
+			break;
+		case 402:
+			produto = new ProdutoTO(402, 20, 50, "Camiseta Azul");
+			break;
+		case 403:
+			produto = new ProdutoTO(403, 30, 20, "Camiseta Rosa");
+			break;
+		default:
+			throw new AxisFault("Produto Não Cadastrado");
+		}
+		return produto;
+	}
+	
+}
